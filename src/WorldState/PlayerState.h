42c39bb763573b6e717802730d066904fbabf805
SteveTownsend <SteveTownsend0@gmail.com>
2020-07-11 14:46:32 -0400

    v1.7.0.0. RC commit Give all the code GPLv3 license boilerplate Add finer-grain profiling for Collections load

--- src/WorldState/PlayerState.h
+++ src/WorldState/PlayerState.h
@@ -1,3 +1,22 @@
+/*************************************************************************
+SmartHarvest SE
+Copyright (c) Steve Townsend 2020
+
+>>> SOURCE LICENSE >>>
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation (www.fsf.org); either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+A copy of the GNU General Public License is available at
+http://www.fsf.org/licensing/licenses
+>>> END OF LICENSE >>>
+*************************************************************************/
 #pragma once
 
 #include "WorldState/LocationTracker.h"
