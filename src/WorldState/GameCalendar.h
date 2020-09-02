44643eaeb1d520f271cc94c522c21da772b3dc39
SteveTownsend <SteveTownsend0@gmail.com>
2020-09-02 13:13:52 -0400

    Printable in-game Date/Time ready for further testing with timeline introspection

--- src/WorldState/GameCalendar.h
+++ src/WorldState/GameCalendar.h
@@ -32,28 +32,21 @@ public:
 	std::string DateTimeString(const float gameTime) const;
 
 private:
-	constexpr unsigned int DaysPerYear();
+	constexpr unsigned int DaysPerYear() const;
 	// start date is 4E 201, 17th of Last Seed
 	/* start time logged in new game is 9.00 am or so:
 		2020-09-01 16:59:23.823     info  18628 GameTime is now 0.375/0.375352
 	*/
 
-	constexpr unsigned int StartYear() {
-		return 201;
-	};
-	constexpr unsigned int StartDay() {
-		return 17;
-	};
+	static constexpr unsigned int StartYear = 201;
+	static constexpr unsigned int StartDay = 17;
 	// zero-based offset
-	constexpr unsigned int StartMonth() {
-		return 7;
-	};
-	constexpr const char* DateTimeFormat() {
-		return "{H}.{m} {p} on {W}, {D}{s} of {M} in 4E {Y}";
-	}
+	static constexpr unsigned int StartMonth = 7;
+
 	static std::unique_ptr<GameCalendar> m_instance;
 	static const std::vector<std::pair<std::string, unsigned int>> m_monthDays;
 	static const std::vector<std::string> m_dayNames;
+	static const std::vector<std::string> m_dayOfMonth;
 };
 
 }
