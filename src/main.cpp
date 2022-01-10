68b697aa9e49626a3b1f07983b3e4754f5736311
SteveTownsend <SteveTownsend0@gmail.com>
2022-01-10 09:25:16 -0500

    v4.1.0.1 final German translation courtesy of Nexus user rore58

--- src/main.cpp
+++ src/main.cpp
@@ -235,7 +235,7 @@ extern "C" DLLEXPORT constinit auto SKSEPlugin_Version = []() { {
 		SKSE::PluginVersionData v;
 
 		// WET WET WET but less work than injecting Version in the build a la Quick Loot RE
-		v.PluginVersion({ 4, 1, 0, 0 });
+		v.PluginVersion({ 4, 1, 0, 1 });
 		v.PluginName(SHSE_NAME);
 		v.AuthorName(MOD_AUTHOR);
 		v.AuthorEmail(MOD_SUPPORT);
