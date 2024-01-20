7c4f86fb253abada4c70ed024ddb3d6b6730c324
Steve Townsend <stownsend@ntglobalmarkets.com>
2024-01-20 11:11:16 -0500

    Remove old batch jobs to build the Nexus package

--- Batch/build-Logging-artifacts-rar.bat
+++
@@ -1,3 +0,0 @@
-@if exist "S:\github\x64\Logging\Smart Harvest SE Logging-x.x.x.x.rar" (del "S:\github\x64\Logging\Smart Harvest SE Logging-x.x.x.x.rar")
-
-@"C:\Program Files\WinRAR\rar.exe" a -ep -apSKSE/Plugins "j:/github/x64/Logging/Smart Harvest SE Logging-x.x.x.x.rar" j:/github/x64/Logging/SmartHarvestSE.dll
